9b0fca8ef0f60d3e5f0e8bbe32f695daa20f26cc
Vijayan1606 <muthuvijayan1606@gmail.com>
2024-09-15 14:36:29 +0530

    Food_Order_Project

---
+++ frontend/src/components/layouts/Home.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect } from 'react'
+import CountRestaurant from './CountRestaurant'
+import Restaurant from './Restaurant';
+import {useDispatch, useSelector} from "react-redux"
+import { getRestaurants, sortByRatings, sortByReviews, toggleVegOnly } from '../../actions/restaurantAction';
+import Loader from "./Loader";
+import Message from './Message';
+
+export default function Home() {
+const dispatch = useDispatch();
+
+const { loading:restaurantsLoding,
+   error: restaurantsError,
+    restaurants, showVegOnly} = useSelector
+    ((state)=> state.restaurants);
+
+useEffect(() => {
+  dispatch(getRestaurants());
+},[dispatch]);
+
+const handleSortByReview = () => {
+  dispatch(sortByReviews());
+}; 
+
+const handleSortByRatings = () => {
+  dispatch(sortByRatings());
+};
+
+const handleToggleVegOnly = () => {
+  dispatch(toggleVegOnly());
+};
+
+  return (
+    <>
+      <CountRestaurant/>
+      {restaurantsLoding ? (<Loader/>): restaurantsError ? (<Message varient = "danger">
+        {restaurantsError}</Message>):
+      (
+        <>
+          <section>
+        <div className="sort">
+            <button className="sort_veg p-3" onClick={handleToggleVegOnly}>
+              {showVegOnly ? "Show All" : "Pure Veg"}
+            </button>
+            <button className="sort_rev p-3" onClick={handleSortByReview}>Sort By Review</button>
+            <button className="sort_rate p-3"onClick={handleSortByRatings}>Sort By Ratings</button>
+        </div>
+        <div className="row mt-4">
+         {restaurants  ? (restaurants.map((restaurant)=>
+         !showVegOnly || (showVegOnly && restaurant.isVeg)?
+         (
+          <Restaurant key={restaurant._id} restaurant={restaurant}/>
+         ):null)
+        ):(<Message varient="info">No Restaurant Found</Message>)}
+        </div>
+      </section>
+
+
+        </>
+      )}
+      
+    </>
+  );
+}
---
+++ frontend/src/components/layouts/Search.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {FaSearch} from "react-icons/fa";
+
+export default function Search() {
+  return (
+    <form >
+        <div className="input-group">
+            <input
+             type="text" 
+             placeholder='Search your favourite Restaurant.....'
+             id='search-field'
+             className="form-control" />
+             <div className="input-group-append">
+                <button className="btn" id='search_btn'>
+                    <FaSearch className='fa fa-search'/>
+                </button>
+             </div>
+        </div>
+    </form>
+  );
+}
